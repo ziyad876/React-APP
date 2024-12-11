324bbc2d573a3a915bc4447cb9b62bb48a5ca580
Muhammed Siyad <132431934+ziyad876@users.noreply.github.com>
2024-12-11 11:16:31 +0530

    updat3

--- src/Components/SlideImage.jsx
+++ src/Components/SlideImage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Slide(){
     return <div id="myCarousel" className="col-lg-12 col-12 col-md-12 col-sm-12 carousel slide ImgSlide" data-bs-ride="carousel">
-                <img src="asssets/image.png" alt=""/>
+                <img src="https://img.freepik.com/free-photo/observation-urban-building-business-steel_1127-2397.jpg" alt=""/>
             </div>
 }
 
